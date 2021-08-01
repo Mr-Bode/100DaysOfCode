7a4cabd2237478826b4856a4f984d31b661a76a4
Bode <bode7000@gmail.com>
2021-08-01 12:43:23 +0100

    calculator

---
+++ Day_10/calculator.py
@@ -0,0 +1,68 @@
+#Calculator
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+#Add
+def add(n1, n2):
+  return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+  return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+#Divide
+def divide(n1, n2):
+  return n1 / n2
+
+#Dictionary
+operations = {
+  "+": add, 
+  "-": subtract, 
+  "*": multiply, 
+  "/": divide,
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
+
+  for sign in operations:
+    print(sign)
+    should_continue = True
+
+  while should_continue:
+    operation_sign = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_sign]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_sign} {num2} = {answer}")
+
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
